aeb42d4cb5fd6df8f624f79c8aa352290f9d68e8
Osman Orhan Askin <115871580+osmanAskin@users.noreply.github.com>
2023-11-11 13:54:13 +0300

    Dönüştürme

---
+++ BinaryDönüstürmeAlgoritması/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("İkilik Tabandaki Sayıyı Giriniz : ");
+
+string ikiliSay = Console.ReadLine();
+
+int onluSay = Convert.ToInt32(ikiliSay ,2);
+
+Console.WriteLine("Onlu Sayı Sistemi: " + onluSay);
+
